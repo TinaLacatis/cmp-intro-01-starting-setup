cc326fc9d83967518276183a1caf5f29a9c21d1b
Lacatis, Danuta Tina <danutatina.lacatis@iso-gruppe.com>
2023-11-11 12:28:15 +0100

    add the list dinamically, activate the hide/ show button

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      detailsAreVisible: false,
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123456789",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0123456789",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.mount("#app");
